f1dcfb349181e3b6908342047ab9e93069e958b4
lisopin <76459714+edsw0922@users.noreply.github.com>
2026-01-22 17:29:53 +0900

    Update File Server

--- app/assets/js/distromanager.js
+++ app/assets/js/distromanager.js
@@ -4,7 +4,7 @@ const ConfigManager = require('./configmanager')
 
 // Old WesterosCraft url.
 // exports.REMOTE_DISTRO_URL = 'http://mc.westeroscraft.com/WesterosCraftLauncher/distribution.json'
-exports.REMOTE_DISTRO_URL = 'http://cdn.lisopin.kr:9222/distribution.json'
+exports.REMOTE_DISTRO_URL = 'https://cdn.lisopin.kr/distribution.json'
 
 const api = new DistributionAPI(
     ConfigManager.getLauncherDirectory(),
